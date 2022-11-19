0159c13cc52ee44267aa7e122b45e76beac60c81
Saranms607 <113238983+Saranms607@users.noreply.github.com>
2022-11-19 11:29:04 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint_2/app.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template,url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
+
+@app.route("/dashboard")
+def signup():
+    return render_template('dashboard.html')    
+
